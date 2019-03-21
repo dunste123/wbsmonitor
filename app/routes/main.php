5e75ec8156b7b6ea616de2a96a15c9b1ac8ade26
Duncan Sterken <duncan.sterken123@gmail.com>
2019-03-21 11:46:40 +0100

    YEET

--- app/routes/main.php
+++ app/routes/main.php
@@ -8,6 +8,16 @@ $router->get('/', function (Twig $twig) {
 
     return $twig->render('index.twig', [
         'title' => 'Home',
+        'active' => 'home',
+    ]);
+
+});
+
+$router->get('/second', function (Twig $twig) {
+
+    return $twig->render('second_page.twig', [
+        'title' => 'Another Page OwO',
+        'active' => 'second',
     ]);
 
 });
